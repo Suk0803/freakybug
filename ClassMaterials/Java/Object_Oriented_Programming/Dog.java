781ac76da9650ccb9c0102ab609dd4da32772d8a
JayLee_83 <50763770+Suk0803@users.noreply.github.com>
2023-05-13 22:02:49 +0900

    end class 02

---
+++ ClassMaterials/Java/Object_Oriented_Programming/Dog.java
@@ -0,0 +1,25 @@
+package ClassMaterials.Java.Object_Oriented_Programming;
+
+public class Dog {
+    private String name;
+    private int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public static void main(String[] args) {
+        Dog myDog = new Dog("Max", 5);
+        System.out.println("My dog's name is " + myDog.getName() + " and he is " + myDog.getAge() + " years old.");
+    }
+
+}
